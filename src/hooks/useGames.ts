1d7729268f28658a4fbb06e1d03273f7503eca8f
iRE-X <harekrishna8016@gmail.com>
2023-11-13 20:03:40 +0530

    Refactor : Simplify Game-Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,15 +20,15 @@ const useGames = (gameQuery: GameQuery) => {
         queryFn: ({ pageParam }) =>
             apiClient.getAll({
                 params: {
-                    genres: gameQuery.genre?.id,
-                    parent_platforms: gameQuery.platform?.id,
+                    genres: gameQuery.genreId,
+                    parent_platforms: gameQuery.platformId,
                     ordering: gameQuery.sortOrder,
                     search: gameQuery.searchText,
                     page: pageParam,
                 },
             }),
         initialPageParam: 1,
-        staleTime: 1 * 60 * 60 * 1000, // 1hour
+        staleTime: 24 * 60 * 60 * 1000, // 1hour
         getNextPageParam: (lastpage, allpages) => {
             return lastpage.length ? allpages.length + 1 : undefined;
         },
