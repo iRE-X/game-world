26d07bf4265b433fc73a6ac2c74e13a750ae9ebf
iRE-X <harekrishna8016@gmail.com>
2023-11-22 09:18:46 +0530

    Fix NavBar

--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -1,5 +1,5 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import DefinitionItem from "./DefinitionItem";
 import CriticScore from "./CriticScore";
 
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -14,7 +14,7 @@ const GameDetailsPage = () => {
     if (error || !game) throw error;
 
     return (
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5} paddingX={5}>
             <GridItem>
                 <Heading>{game.name}</Heading>
                 <ExpandableText>{game.description_raw}</ExpandableText>
